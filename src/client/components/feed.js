c4a9b5cb0eed235a6ac16b9968be7a24dd4d4f20
Max Franz <maxkfranz@gmail.com>
2020-04-27 15:48:27 -0400

    Basic output of test result & symptoms severity in the feed

--- src/client/components/feed.js
+++ src/client/components/feed.js
@@ -33,7 +33,11 @@ export class Feed extends Component {
       const relativeDate = formatDistance(date, now) + ' ago';
       const absoluteDate = format(date, 'yyyy-MM-dd');
       const { age, sex } = entry.patient_information;
-      const { fever, cough, fatigue } = entry.symptoms;
+      const { fever, cough, fatigue, severity } = entry.symptoms;
+      const { result, score: statusScore } = entry.test_information;
+      const symptomsScore = (severity === 'mild' ? 0.25 : (severity === 'moderate' ? 0.5 : 1));
+
+      console.log(entry)
       
       const Symptom = ({ hasSymptom, icon }) => h('div.feed-symptom', {
         className: classNames({ 'feed-symptom-true': hasSymptom })
@@ -43,26 +47,34 @@ export class Feed extends Component {
 
       return h('div.feed-entry', [
         h('div.feed-title', [
+          h('div.feed-status', [
+            h(`div.feed-status-result.feed-status-result-${result ? 'positive' : 'negative'}`, [
+              h(`i.fas.fa-fw.fa-${result ? 'virus' : 'virus-slash'}`)
+            ]),
+            h('div.feed-status-score', [
+              h('div.feed-status-score-bar', { style: { width: `${100 * statusScore}%` } })
+            ])
+          ]),
           h('div.feed-patient', [
             h('div.feed-patient-descr', [
               h('div.feed-age', age),
               h('div.feed-sex', sex)
             ]),
-            h('div.feed-symptoms', [
-              h(Symptom, { hasSymptom: fever, icon: 'thermometer-three-quarters' }),
-              h(Symptom, { hasSymptom: cough, icon: 'head-side-cough' }),
-              h(Symptom, { hasSymptom: fatigue, icon: 'bed' })
-            ]),
+            h('.div.feed-symptoms-info', [
+              h('div.feed-symptoms', [
+                h(Symptom, { hasSymptom: fever, icon: 'thermometer-three-quarters' }),
+                h(Symptom, { hasSymptom: cough, icon: 'head-side-cough' }),
+                h(Symptom, { hasSymptom: fatigue, icon: 'bed' })
+              ]),
+              h('div.feed-symptoms-score', [
+                h('div.feed-symptoms-score-bar', { style: { width: `${100 * symptomsScore}%` } })
+              ])
+            ])
           ]),
           h('div.feed-date', [
             h('div.feed-date-relative', relativeDate),
             h('div.feed-date-absolute', absoluteDate)
           ]),
-        ]),
-        h('div.feed-tests', [
-          h('div.feed-test', { style: { width: '100%' } }),
-          h('div.feed-test', { style: { width: '66%' } }),
-          h('div.feed-test', { style: { width: '33%' } })
         ])
       ]);
     }));
